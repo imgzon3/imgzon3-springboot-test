e15b27a56b04e5114ffacaf58054666e75546f84
imgzon3 <imgzon3@naver.com>
2021-07-06 22:17:28 +0900

    .

--- src/main/java/com/imgzon3/test/springboot/Application.java
+++ src/main/java/com/imgzon3/test/springboot/Application.java
@@ -3,7 +3,7 @@ package com.imgzon3.test.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// 메인 클래스 시작하기
+// 메인 클래스 시작
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
