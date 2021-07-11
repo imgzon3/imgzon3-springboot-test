cd342df4cfd9b3eec2f1e1e938cfe07c8ac14b53
imgzon3 <imgzon3@naver.com>
2021-07-11 19:16:19 +0900

    change HelloResponseDto.java

--- src/main/java/com/imgzon3/test/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/imgzon3/test/springboot/web/dto/HelloResponseDto.java
@@ -1,4 +1,12 @@
 package com.imgzon3.test.springboot.web.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter // 선언한 모든 필드의 get 생성해줌
+@RequiredArgsConstructor // 선언된 모든 "final"붙은 필드의 생성자 생성해 줌
 public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
 }
