d9207bc155710b72b168eddb3def8773a19aea09
imgzon3 <imgzon3@naver.com>
2021-07-15 21:42:21 +0900

    등록 api 구현하기

--- src/main/java/com/imgzon3/test/springboot/service/posts/PostsService.java
+++ src/main/java/com/imgzon3/test/springboot/service/posts/PostsService.java
@@ -1,4 +1,19 @@
 package com.imgzon3.test.springboot.service.posts;
 
+import com.imgzon3.test.springboot.domain.posts.PostsRepository;
+import com.imgzon3.test.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
