b421602392ebdcea9c2c58aa3986d9c05539fd79
imgzon3 <imgzon3@naver.com>
2021-07-19 22:46:20 +0900

    add method Posts_수정된다

--- src/test/java/com/imgzon3/test/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/imgzon3/test/springboot/web/PostsApiControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -83,11 +84,23 @@ public class PostsApiControllerTest {
                 .content(expectedContent)
                 .build();
 
-        String url = "http://localhost:"+port+"api/v1/posts/"+updateId;
+        String url = "http://localhost:"+port+"/api/v1/posts/"+updateId;
 
         HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
 
         //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
 
+        //then
+        assertThat(responseEntity.getStatusCode())
+                .isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody())
+                .isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle())
+                .isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent())
+                .isEqualTo(expectedContent);
     }
 }
