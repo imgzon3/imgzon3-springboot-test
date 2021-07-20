b5453e51f69843fde382564c370a0728a95c2497
imgzon3 <imgzon3@naver.com>
2021-07-20 18:02:58 +0900

    @EnableJpaAuditing 추가

--- src/main/java/com/imgzon3/test/springboot/Application.java
+++ src/main/java/com/imgzon3/test/springboot/Application.java
@@ -2,8 +2,10 @@ package com.imgzon3.test.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // 메인 클래스 시작
+@EnableJpaAuditing // JPA Auditing 어노테이션들 모두 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
