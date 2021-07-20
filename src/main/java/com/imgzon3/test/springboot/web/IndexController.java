1b1235845c8a18522b5eda75f8af9e23e4a22c7f
imgzon3 <imgzon3@naver.com>
2021-07-20 21:08:33 +0900

    IndexController 추가, method index() 추가

---
+++ src/main/java/com/imgzon3/test/springboot/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.imgzon3.test.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//URL 매핑해주는 컨트롤러
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";// to index.mustache
+    }
+}
