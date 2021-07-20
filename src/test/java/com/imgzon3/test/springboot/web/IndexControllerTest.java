877d63dddf87931078b74516a143884712409295
imgzon3 <imgzon3@naver.com>
2021-07-20 21:56:29 +0900

    메인페이지_로딩() 테스트코드 추가

---
+++ src/test/java/com/imgzon3/test/springboot/web/IndexControllerTest.java
@@ -0,0 +1,31 @@
+package com.imgzon3.test.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class); // HTML 가져오는 듯
+
+//        System.out.println("========================================================");
+//        System.out.println(body);
+//        System.out.println("========================================================");
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
