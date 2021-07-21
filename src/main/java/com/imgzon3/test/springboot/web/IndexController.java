5c49b4bdfc2385cd53d01eaf9f841db9d9116448
imgzon3 <imgzon3@naver.com>
2021-07-21 17:35:06 +0900

    index()변경

--- src/main/java/com/imgzon3/test/springboot/web/IndexController.java
+++ src/main/java/com/imgzon3/test/springboot/web/IndexController.java
@@ -1,16 +1,23 @@
 package com.imgzon3.test.springboot.web;
 
+import com.imgzon3.test.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //URL 매핑해주는 컨트롤러
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
-    }// to index.mustache
+    }
 
     @GetMapping("/posts/save")
     public String postsSave() {
